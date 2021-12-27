879e01d5f97d200bdf1fa02f4bb488c4f16c4cf1
Hordiychuk Vitaliy <vitalikokgo@gmail.com>
2021-12-27 17:05:57 +0300

    .

---
+++ lab3/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿
+namespace lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.treeView2 = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.проПрограмуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.treeView2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.listBox2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox4, 1, 1);
+            this.tableLayoutPanel1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 85F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(422, 422);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // treeView2
+            // 
+            this.treeView2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.treeView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView2.ImageIndex = 0;
+            this.treeView2.ImageList = this.imageList1;
+            this.treeView2.Location = new System.Drawing.Point(3, 3);
+            this.treeView2.Name = "treeView2";
+            this.treeView2.SelectedImageIndex = 0;
+            this.treeView2.Size = new System.Drawing.Size(205, 352);
+            this.treeView2.TabIndex = 0;
+            this.treeView2.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView2_BeforeExpand);
+            this.treeView2.BeforeSelect += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView2_BeforeSelect);
+            this.treeView2.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView2_NodeMouseClick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "folder-icons.png");
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(3, 361);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(151, 28);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            this.comboBox2.Click += new System.EventHandler(this.comboBox2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(214, 3);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(205, 352);
+            this.listBox2.TabIndex = 2;
+            this.listBox2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBox2_MouseClick);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(214, 361);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(151, 28);
+            this.comboBox4.TabIndex = 3;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Cursor = System.Windows.Forms.Cursors.VSplit;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 28);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tableLayoutPanel2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tableLayoutPanel1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 422);
+            this.splitContainer1.SplitterDistance = 374;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.comboBox3, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.treeView1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.listBox1, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.comboBox1, 0, 1);
+            this.tableLayoutPanel2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 85F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(374, 422);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(190, 361);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(151, 28);
+            this.comboBox3.TabIndex = 4;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(3, 3);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(181, 352);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeExpand);
+            this.treeView1.BeforeSelect += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeSelect);
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(190, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(181, 352);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseClick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(3, 361);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 28);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.Click += new System.EventHandler(this.comboBox1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.проПрограмуToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // проПрограмуToolStripMenuItem
+            // 
+            this.проПрограмуToolStripMenuItem.Name = "проПрограмуToolStripMenuItem";
+            this.проПрограмуToolStripMenuItem.Size = new System.Drawing.Size(124, 24);
+            this.проПрограмуToolStripMenuItem.Text = "Про програму";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TreeView treeView2;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem проПрограмуToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ComboBox comboBox4;
+    }
+}
+
---
+++ lab3/Form1.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Xml;
+
+
+namespace lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SetComboBox(comboBox1);
+            comboBox1.Text = comboBox1.Items[0].ToString();
+            SetComboBox(comboBox2);
+            comboBox2.Text = comboBox2.Items[0].ToString();;
+        }
+        private List<Files> files = new List<Files>();
+        private List<Files> FILES = new List<Files>();
+        private class Files
+        {
+            public string Path { get; set; }
+            public string Name { get; set; }
+            public Files() { }
+            public Files(string p,string n)
+            {
+                Path = p;
+                Name = n;
+            }
+        }
+        private static class State
+        {
+            public static string ComboBox3;
+            public static string ComboBox4;
+        }
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            BeforeSelect(e);
+        }
+        private void Drives(dynamic treeView, string drive)
+        {
+            try
+            {
+                TreeNode d = new TreeNode {Text=drive };
+                FillNode(d, drive);
+                treeView.Nodes.Add(d);
+            }
+            catch (Exception ex) {}
+        }
+        private void FillNode(TreeNode driveNode, string path)
+        {
+            try
+            {
+                string[] dirs = Directory.GetDirectories(path);
+                foreach (string dir in dirs)
+                {
+                    TreeNode dirNode = new TreeNode();
+                    dirNode.Text = dir.Remove(0,dir.LastIndexOf("\\")+1);
+                    driveNode.Nodes.Add(dirNode);
+                }
+            }
+            catch (Exception ex) { }
+        }
+        private void treeView1_BeforeSelect(object sender, TreeViewCancelEventArgs e)
+        {
+            BeforeSelect(e);
+        }
+        private void BeforeSelect(dynamic e )
+        {
+            e.Node.Nodes.Clear();
+            string[] dirs;
+            try
+            {
+                if (Directory.Exists(e.Node.FullPath))
+                {
+                    dirs = Directory.GetDirectories(e.Node.FullPath);
+                    if (dirs.Length > 0)
+                    {
+                        for (int i = 0; i < dirs.Length; i++)
+                        {
+                            TreeNode dirNode = new TreeNode(new DirectoryInfo(dirs[i]).Name);
+                            FillNode(dirNode, dirs[i]);
+                            e.Node.Nodes.Add(dirNode);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { }
+        }
+        public void SetComboBox(dynamic comboBox)
+        {
+            comboBox.Items.Clear();
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (var d in drives)
+            {
+                comboBox.Items.Add(d.Name);
+            }
+        }     
+        private void comboBox1_Click(object sender, EventArgs e)
+        {
+            SetComboBox(comboBox1);
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+            Drives(treeView1,comboBox1.Text);
+        }
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            treeView2.Nodes.Clear();
+            Drives(treeView2, comboBox2.Text);
+        }
+        private void comboBox2_Click(object sender, EventArgs e)
+        {
+            SetComboBox(comboBox2);
+        }
+        private void treeView2_BeforeSelect(object sender, TreeViewCancelEventArgs e)
+        {
+            BeforeSelect(e);
+        }
+
+        private void treeView2_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            BeforeSelect(e);
+        }
+
+        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            comboBox3.Items.Clear();
+            files.Clear();
+            try
+            {
+                if ((Directory.GetFiles(e.Node.FullPath)).Length > 0)
+                {
+                    foreach (string f in Directory.GetFiles(e.Node.FullPath))
+                    {
+                        FileInfo file1 = new FileInfo(f);
+                        if (comboBox3.Items.Contains(file1.Extension) == false)
+                        {
+                            comboBox3.Items.Add(file1.Extension);
+                        }
+                        Files file = new Files(f, f.Remove(0, f.LastIndexOf("\\") + 1));
+                        files.Add(file);
+                    }
+                }
+                List<Files> files1 = new List<Files>();
+                foreach (var f in files)
+                {
+                    FileInfo t = new FileInfo(f.Path);
+                    if (t.Extension == State.ComboBox3)
+                    {
+                        files1.Add(f);
+                    }
+                }
+                listBox1.DataSource = files1;
+                listBox1.DisplayMember = "Name";
+                listBox1.ValueMember = "Path";
+            }
+            catch (Exception ex) 
+            {
+                string[] s = new string[] { "Немає доступу" };
+                listBox1.DataSource =s;
+                listBox1.DisplayMember = null;
+                listBox1.ValueMember = null;
+                comboBox3.Text = "";
+                State.ComboBox3 = "";
+            }
+            
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            State.ComboBox3 = comboBox3.Text;
+            listBox1.Text="";
+            try
+            {
+                List<Files> files1 = new List<Files>();
+                foreach (var f in files)
+                {
+                    FileInfo t = new FileInfo(f.Path);
+                    if (t.Extension == State.ComboBox3)
+                    {
+                        files1.Add(f);
+                    }
+                }
+                listBox1.DataSource = files1;
+                listBox1.DisplayMember = "Name";
+                listBox1.ValueMember = "Path";
+            }
+            catch (Exception ex)
+            {
+                string[] s = new string[] { "Немає доступу" };
+                listBox1.DataSource = s;
+                listBox1.DisplayMember = null;
+                listBox1.ValueMember = null;
+                comboBox3.Text = "";
+                State.ComboBox3 = "";
+            }
+        }
+
+        private void treeView2_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            comboBox4.Items.Clear();
+            FILES.Clear();
+            try
+            {
+                if ((Directory.GetFiles(e.Node.FullPath)).Length > 0)
+                {
+                    foreach (string f in Directory.GetFiles(e.Node.FullPath))
+                    {
+                        FileInfo file1 = new FileInfo(f);
+                        if (comboBox4.Items.Contains(file1.Extension) == false)
+                        {
+                            comboBox4.Items.Add(file1.Extension);
+                        }
+                        Files file = new Files(f, f.Remove(0, f.LastIndexOf("\\") + 1));
+                        FILES.Add(file);
+                    }
+                }
+                List<Files> files1 = new List<Files>();
+                foreach (var f in FILES)
+                {
+                    FileInfo t = new FileInfo(f.Path);
+                    if (t.Extension == State.ComboBox4)
+                    {
+                        files1.Add(f);
+                    }
+                }
+                listBox2.DataSource = files1;
+                listBox2.DisplayMember = "Name";
+                listBox2.ValueMember = "Path";
+            }
+            catch (Exception ex)
+            {
+                string[] s = new string[] { "Немає доступу" };
+                listBox2.DataSource = s;
+                listBox2.DisplayMember = null;
+                listBox2.ValueMember = null;
+                comboBox4.Text = "";
+                State.ComboBox4 = "";
+            }
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            State.ComboBox4 = comboBox4.Text;
+            listBox2.Text = "";
+            try
+            {
+                List<Files> files1 = new List<Files>();
+                foreach (var f in FILES)
+                {
+                    FileInfo t = new FileInfo(f.Path);
+                    if (t.Extension == State.ComboBox4)
+                    {
+                        files1.Add(f);
+                    }
+                }
+                listBox2.DataSource = files1;
+                listBox2.DisplayMember = "Name";
+                listBox2.ValueMember = "Path";
+            }
+            catch (Exception ex)
+            {
+                string[] s = new string[] { "Немає доступу" };
+                listBox2.DataSource = s;
+                listBox2.DisplayMember = null;
+                listBox2.ValueMember = null;
+                comboBox4.Text = "";
+                State.ComboBox4 = "";
+            }
+        }
+
+        private void listBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            FileInfo f = new FileInfo(listBox1.SelectedValue.ToString());
+            if (f.Extension == ".html")
+            {
+                HTMLProcessing(listBox1.SelectedValue.ToString());
+                return;
+            }
+            try
+            {
+                Process.Start(new ProcessStartInfo(listBox1.SelectedValue.ToString()) { UseShellExecute = true });
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void listBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo(listBox2.SelectedValue.ToString()) { UseShellExecute = true });
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void HTMLProcessing(string path)
+        {
+         
+
+
+        }
+    }
+}
